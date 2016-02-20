75766d5f3baf0a53aa7bf0ae62241398a3d0f593
gj <gj@itcast.cn>
2016-02-20 11:13:38 +0800

    初始化项目

---
+++ LZWeiBo/LZWeiBo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LZWeiBo
+//
+//  Created by apple on 16/2/20.
+//  Copyright © 2016年 小码哥. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LZWeiBo/LZWeiBo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  LZWeiBo
+//
+//  Created by apple on 16/2/20.
+//  Copyright © 2016年 小码哥. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
