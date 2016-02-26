d8240af3fde1b0b70c0acb38689a5f1a8c70f930
gj <gj@itcast.cn>
2016-02-27 01:16:54 +0800

    添加子控制器

---
+++ LZWeiBo/LZWeiBo/Classes/LZDiscoverViewController.h
@@ -0,0 +1,13 @@
+//
+//  LZDiscoverViewController.h
+//  LZWeiBo
+//
+//  Created by apple on 16/2/27.
+//  Copyright © 2016年 m14a. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LZDiscoverViewController : UITableViewController
+
+@end
---
+++ LZWeiBo/LZWeiBo/Classes/LZMessageCenterViewController.h
@@ -0,0 +1,13 @@
+//
+//  LZMessageCenterViewController.h
+//  LZWeiBo
+//
+//  Created by apple on 16/2/27.
+//  Copyright © 2016年 m14a. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LZMessageCenterViewController : UITableViewController
+
+@end
