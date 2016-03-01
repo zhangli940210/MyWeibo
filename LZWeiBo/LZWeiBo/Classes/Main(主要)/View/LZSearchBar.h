4f2ca52a7f1336354e7f818d8c6a4a3e4c46519c
gj <gj@itcast.cn>
2016-03-01 11:38:40 +0800

    自定义搜索框

---
+++ LZWeiBo/LZWeiBo/Classes/Main(主要)/View/LZSearchBar.h
@@ -0,0 +1,13 @@
+//
+//  LZSearchBar.h
+//  LZWeiBo
+//
+//  Created by apple on 16/3/1.
+//  Copyright © 2016年 m14a. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LZSearchBar : UITextField
++ (instancetype)searchBar;
+@end
