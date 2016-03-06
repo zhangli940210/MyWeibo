a2e2d60854cd7044646502183c183db32cdde4bc
gj <gj@itcast.cn>
2016-03-06 14:26:11 +0800

    修改按钮尺寸

---
+++ LZWeiBo/LZWeiBo/Classes/Home(首页)/View/LZTitleButton.h
@@ -0,0 +1,13 @@
+//
+//  LZTitleButton.h
+//  LZWeiBo
+//
+//  Created by apple on 16/3/6.
+//  Copyright © 2016年 m14a. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LZTitleButton : UIButton
+
+@end
